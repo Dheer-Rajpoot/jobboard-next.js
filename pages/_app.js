01470b44da0d6f7fbda68816022bd53a0aa83a13
Dheer Singh Rajpoot <Dheer.Singh@akqa.com>
2022-09-18 17:35:55 +0530

    added seo for company

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,46 @@
 import "../styles/globals.css";
 import Layout from "../components/globals/Layout";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
+  const { company } = pageProps;
+  const metaData = (seo) => {
+    const metaArr = [];
+    for (const key in seo) {
+      if (seo.enable_search_indexing) {
+        metaArr.push(
+          <meta
+            name={
+              key.includes("meta_")
+                ? key.split("meta_")[1].toString()
+                : key.toString()
+            }
+            content={seo[key].toString()}
+            key={key}
+          />
+        );
+      }
+    }
+    return metaArr;
+  };
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="application-name" content="Job Board App" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1"
+        />
+        <title>Job Board Next.js Contentstack</title>
+        {company?.seo &&
+          company.seo.enable_search_indexing &&
+          metaData(company.seo)}
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
